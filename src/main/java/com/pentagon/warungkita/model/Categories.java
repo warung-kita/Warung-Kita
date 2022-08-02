e7f6650c5c992bcb3f1193b1bb0b09604e88e292
rendraPrames <rendraprames@gmail.com>
2022-08-02 10:19:02 +0700

    First Commit

--- src/main/java/com/pentagon/warungkita/model/Categories.java
+++ src/main/java/com/pentagon/warungkita/model/Categories.java
@@ -3,16 +3,21 @@ package com.pentagon.warungkita.model;
 import lombok.*;
 import javax.persistence.*;
 
-@Setter
 @Getter
-@Entity
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
 @Table(name = "categories")
 public class Categories {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long categoriesId;
 
+    @Column(name = "name")
+    private String name;
 
 }
--- src/main/java/com/pentagon/warungkita/model/Product.java
+++ src/main/java/com/pentagon/warungkita/model/Product.java
@@ -3,14 +3,35 @@ package com.pentagon.warungkita.model;
 import lombok.*;
 import javax.persistence.*;
 
-@Setter
 @Getter
-@Entity
+@Setter
 @NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Builder
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long productId;
+
+    @Column(name = "sku")
+    private String sku;
+
+    @Column(name = "name")
+    private String productName;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "product_status_id")
+    private Integer productStatusId;
+
+    @Column(name = "regular_price")
+    private Integer regularPrice;
+
+    @Column(name = "quantity")
+    private Integer quantity;
 
 }
