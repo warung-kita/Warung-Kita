cedd4229f0c3d895d6cd6cd84c93f044e01c2fa7
Muhammad Rasyid Hidaya <rasyidh55@gmail.com>
2022-08-04 13:16:38 +0700

    LOGGER

--- src/main/java/com/pentagon/warungkita/controller/OrderProductController.java
+++ src/main/java/com/pentagon/warungkita/controller/OrderProductController.java
@@ -9,6 +9,8 @@ import com.pentagon.warungkita.response.ResponseHandler;
 import com.pentagon.warungkita.service.OrderProductService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +22,8 @@ import java.util.*;
 @AllArgsConstructor
 @Slf4j
 public class OrderProductController {
+
+    private static final Logger logger = LogManager.getLogger(OrderProductController.class);
     private OrderProductService orderProductService;
 
     /*
@@ -32,14 +36,32 @@ public class OrderProductController {
         try {
             List<OrderProduct> orderList = orderProductService.getAll();
             List<OrderProductResponseDTO> orderMaps = new ArrayList<>();
+            logger.info("==================== Logger Start Get All Order Product     ====================");
             for (OrderProduct dataOrder : orderList) {
                 Map<String, Object> order = new HashMap<>();
+                order.put("OrderProductID       : ", dataOrder.getOrderProductId());
+                order.put("OrederID       : ", dataOrder.getOrderId());
+                order.put("ProductID : ", dataOrder.getOrderProductId());
+                order.put("SKU    : ", dataOrder.getSku());
+                order.put("ProductName    : ", dataOrder.getProductName());
+                order.put("Description : ", dataOrder.getDescription());
+                order.put("Price : ", dataOrder.getPrice());
+                logger.info("OrderProductID       : " + dataOrder.getOrderProductId());
+                logger.info("OrederID       : " + dataOrder.getOrderId());
+                logger.info("ProductID : " + dataOrder.getProductId());
+                logger.info("SKU    : " + dataOrder.getSku());
+                logger.info("ProductName    : " + dataOrder.getProductName());
+                logger.info("Description : " + dataOrder.getDescription());
+                logger.info("Price : " + dataOrder.getPrice());
                 OrderProductResponseDTO orderResponseDTO = dataOrder.convertToResponse();
                 orderMaps.add(orderResponseDTO);
             }
+            logger.info("==================== Logger Start Get All Order Product     ====================");
             return ResponseHandler.generateResponse("Successfully  getAll data!", HttpStatus.OK, orderMaps);
         } catch (ResourceNotFoundException e) {
-            log.error(e.getMessage());
+            logger.error("------------------------------------");
+            logger.error(e.getMessage());
+            logger.error("------------------------------------");
             return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Table not found");
         }
     }
@@ -50,9 +72,14 @@ public class OrderProductController {
             Optional<OrderProduct> orderList = orderProductService.getOrderProductById(orderProductId);
             OrderProduct orderProduct = orderList.get();
             OrderProductResponseDTO orderResponseDTO = orderProduct.convertToResponse();
+            logger.info("==================== Logger Start Get Order Product By ID ====================");
+            logger.info(orderResponseDTO);
+            logger.info("==================== Logger End Get Order Product By ID =================");
             return ResponseHandler.generateResponse("Success Get By Id", HttpStatus.OK, orderResponseDTO);
         } catch (ResourceNotFoundException e) {
-            log.error(e.getMessage());
+            logger.error("------------------------------------");
+            logger.error(e.getMessage());
+            logger.error("------------------------------------");
             return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "ID not found");
         }
     }
@@ -63,9 +90,14 @@ public class OrderProductController {
             OrderProduct orderProductSave = orderProductRequestDTO.convertToEntity();
             OrderProduct orderProduct = orderProductService.saveOrderProduct(orderProductSave);
             OrderProductResponsePOST responsePOST = orderProduct.convertToResponsePOST();
+            logger.info("==================== Logger Start Post Order Product ====================");
+            logger.info(responsePOST);
+            logger.info("==================== Logger Start Post Order Product =================");
             return ResponseHandler.generateResponse("Successfully  save Order", HttpStatus.CREATED, responsePOST);
         } catch (ResourceNotFoundException e) {
-            log.error(e.getMessage());
+            logger.error("------------------------------------");
+            logger.error(e.getMessage());
+            logger.error("------------------------------------");
             return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Bad Request!!");
         }
     }
@@ -77,9 +109,14 @@ public class OrderProductController {
             orderProduct.setOrderProductId(orderProductId);
             OrderProduct responseUpdate = orderProductService.updateOrderProduct(orderProduct);
             OrderProductResponseDTO responseDTO = responseUpdate.convertToResponse();
+            logger.info("==================== Logger Start Update Order Product By ID ====================");
+            logger.info(responseDTO);
+            logger.info("==================== Logger End Update Order Product By ID =================");
             return ResponseHandler.generateResponse("Data Updated!", HttpStatus.CREATED, responseDTO);
         } catch (ResourceNotFoundException e) {
-            log.error(e.getMessage());
+            logger.error("------------------------------------");
+            logger.error(e.getMessage());
+            logger.error("------------------------------------");
             return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Bad Request");
         }
     }
@@ -89,9 +126,14 @@ public class OrderProductController {
         try {
             orderProductService.deleteOrderProductById(orderProductId);
             Boolean result = Boolean.TRUE;
+            logger.info("==================== Logger Start Delete Order Product By ID ====================");
+            logger.info(result);
+            logger.info("==================== Logger End Delete Order Product By ID =================");
             return ResponseHandler.generateResponse("Success Delete Booking by ID", HttpStatus.OK, result);
         } catch (ResourceNotFoundException e) {
-            log.error(e.getMessage());
+            logger.error("------------------------------------");
+            logger.error(e.getMessage());
+            logger.error("------------------------------------");
             return ResponseHandler.generateResponse(e.getMessage(), HttpStatus.NOT_FOUND, "Data Not Found!");
         }
     }
