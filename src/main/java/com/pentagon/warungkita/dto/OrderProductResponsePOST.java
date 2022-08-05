a4a0cb0fa6c4500e93a6b2603a8ca2a567ef2638
Muhammad Rasyid Hidaya <rasyidh55@gmail.com>
2022-08-05 09:04:17 +0700

    tostring

--- src/main/java/com/pentagon/warungkita/dto/OrderProductResponsePOST.java
+++ src/main/java/com/pentagon/warungkita/dto/OrderProductResponsePOST.java
@@ -16,4 +16,18 @@ public class OrderProductResponsePOST {
     private Integer price;
     private Integer quantity;
     private Integer subtotal;
+
+    @Override
+    public String toString() {
+        return "OrderProductResponsePOST{" +
+                "orderId=" + orderId +
+                ", productId=" + productId +
+                ", sku='" + sku + '\'' +
+                ", productName='" + productName + '\'' +
+                ", description='" + description + '\'' +
+                ", price=" + price +
+                ", quantity=" + quantity +
+                ", subtotal=" + subtotal +
+                '}';
+    }
 }
