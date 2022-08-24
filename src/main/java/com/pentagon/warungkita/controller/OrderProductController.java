69d686219d30c7e9d11d5664db49e586fc6932d5
Yohanes Crusc <ycrusc@gmail.com>
2022-08-24 11:27:38 +0700

    merge from rasyid

--- src/main/java/com/pentagon/warungkita/controller/OrderProductController.java
+++ src/main/java/com/pentagon/warungkita/controller/OrderProductController.java
@@ -1,29 +1,14 @@
 package com.pentagon.warungkita.controller;
 
 import com.pentagon.warungkita.dto.OrderProductRequestDTO;
-import com.pentagon.warungkita.dto.OrderProductResponseDTO;
-
-import com.pentagon.warungkita.exception.ResourceNotFoundException;
-import com.pentagon.warungkita.model.OrderProduct;
-import com.pentagon.warungkita.model.Product;
-import com.pentagon.warungkita.repository.OrderProductRepo;
-import com.pentagon.warungkita.repository.ProductRepo;
-import com.pentagon.warungkita.repository.ProductStatusRepo;
-import com.pentagon.warungkita.response.ResponseHandler;
 import com.pentagon.warungkita.service.OrderProductService;
-import com.pentagon.warungkita.service.ProductStatusService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.*;
 
 @RequestMapping("/pentagon/warung-kita")
 @RestController
@@ -33,10 +18,7 @@ import java.util.*;
 @Tag(name = "08.Order Product")
 public class OrderProductController {
 
-    private static final Logger logger = LogManager.getLogger(OrderProductController.class);
     private OrderProductService orderProductService;
-    private ProductRepo productRepo;
-    private OrderProductRepo orderProductRepo;
 
     /*
      * Get all Data Order products table
--- src/main/java/com/pentagon/warungkita/service/OrderProductService.java
+++ src/main/java/com/pentagon/warungkita/service/OrderProductService.java
@@ -1,13 +1,8 @@
 package com.pentagon.warungkita.service;
 
 import com.pentagon.warungkita.dto.OrderProductRequestDTO;
-import com.pentagon.warungkita.model.OrderProduct;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import java.util.List;
-import java.util.Optional;
 
 public interface OrderProductService {
     ResponseEntity<Object> OrderListById(Long orderProductId);
